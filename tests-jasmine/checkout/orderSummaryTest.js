c70eaf62e30026a4731baf19577e648a6cd547de
kal-aa <sadkalshayee@gmail.com>
2026-04-13 04:25:56 +0400

    put tests-simple inside tests-jasmine

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -1,5 +1,5 @@
-import { cart, loadFromStorage } from "../../data/cart.js";
-import renderOrderSummary from "../../scripts/checkout/orderSummary.js";
+import { cart, loadFromStorage } from "../../../data/cart.js";
+import renderOrderSummary from "../../../scripts/checkout/orderSummary.js";
 
 describe("test suite: renderOrderSummary", () => {
   const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
